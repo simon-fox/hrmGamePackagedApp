894eff6073739e3f9ead7bd365578c9e5237cbdc
simon-fox <simon@playlablondon.co.uk>
2013-03-10 19:12:25 +0000

    attempting communication

--- engine.js
+++ engine.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
-	var ports = [];
+	
+	var connectionId;
+	var serialDataRead;
+	//set open options for port
+	function onOpen(openInfo) {
+	  connectionId = openInfo.connectionId;
+	};
+	
 	//get list of serial ports
 	chrome.serial.getPorts(function(ports) {
 			  for (var i=0; i<ports.length; i++) {
 				console.log(ports[i]);
 			  }
 	});
+	/*
+	chrome.serial.open("COM4", onOpen, function(){
+		console.log('connection Open');
+	});
+	
+	chrome.serial.read(connectionId, 1, function(ReadInfo){
+		serialDataRead = ReadInfo;
+		console.log(serialDataRead);
+	});
+	*/
+	
 });
